fa30dfbe5f4cf8be67843159ddba0d1d94cce1c1
dideco1 <diegobaro.si@outlook.com>
2022-10-17 10:45:49 -0300

    Mudando usuario no codigo

--- App/config.php
+++ App/config.php
@@ -4,7 +4,7 @@ define('basedir', dirname(__FILE__) . '/../' );
 define('app_views', dirname(__FILE__) . '/View/modules/');
 
 
-$_ENV['db']['host'] = 'localhost:3306';
-$_ENV['db']['user'] = 'root';
+$_ENV['db']['host'] = 'localhost:3307';
+$_ENV['db']['user'] = 'usuario';
 $_ENV['db']['pass'] = '1234';
 $_ENV['db']['database'] = 'db_sistema';
